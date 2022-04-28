56861b0a2b99e1fbfc4a8e7d831187e95fa5639d
Asliddin Ayxanov <asliddinayxanov1@gmail.com>
2022-04-29 00:42:26 +0500

    bot

--- public/bot/bot.php
+++ public/bot/bot.php
@@ -1,6 +1,6 @@
 <?php
-require_once("Telegram.php");
-$telegram=new Telegram('2101715389:AAFHNs02NcuyK9TDe-ZkuYcTCSlrHVlqPEY');
+include ("Telegram.php");
+$telegram=new Telegram("2101715389:AAFHNs02NcuyK9TDe-ZkuYcTCSlrHVlqPEY");
 $chat_id = $telegram->ChatID();
 $content = array('chat_id' => $chat_id, 'text' => 'Test');
 $telegram->sendMessage($content);
